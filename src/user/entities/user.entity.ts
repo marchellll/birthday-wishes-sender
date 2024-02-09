86c02ad65d153c8642b04053f43ec25aee560ad9
Marchell Imanuel <imanuel.marchell@gmail.com>
2024-02-10 02:45:49 +0800

    feat: complete user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import {
+  Column,
+  Index,
+  Model,
+  PrimaryKey,
+  Table,
+  Unique
+} from 'sequelize-typescript';
+
+
+@Table({
+  tableName: 'users',
+  timestamps: true,
+  paranoid: true,
+  underscored: true,
+})
+export class User extends Model {
+  @PrimaryKey
+  id: number;
+
+  @Column
+  firstname: string;
+
+  @Column
+  lastname: string;
+
+  @Column
+  email: string;
+
+  @Column
+  timezone: string;
+
+  @Column
+  birthdate: Date;
+
+  @Column
+  created_at: Date;
+
+  @Column
+  updated_at: Date;
+
+  @Column
+  deleted_at: Date;
+}
